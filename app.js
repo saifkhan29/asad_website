3626079467d4081566aba35e23c208dbee8774e6
saifkhan29 <79800224+saifkhan29@users.noreply.github.com>
2022-01-05 18:22:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -149,56 +149,35 @@ const role = document.querySelector('.role')
 
 const testimonials = [
   {
-    name: 'Ajay Mishra',
-    position: 'Marketing Manager',
+    name: 'Saif Khan',
+    position: 'Front End Developer',
     photo:
       'https://images.unsplash.com/photo-1577375729152-4c8b5fcda381?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=600&q=60',
     text:
       "Great service, efficient communication and a really easy way to get a mortgage with lots of help and support to get the right deal.",
   },
   {
-    name: 'Mohan Shah',
-    position: 'Product Manager',
+    name: 'Azfar Uzair',
+    position: 'Software Engineer',
     photo: 'https://images.unsplash.com/photo-1472213984618-c79aaec7fef0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=600&q=60',
     text:
-      '“Our experience has been a very positive one. Working with a management team that is accommodating and sensitive to our needs. What a concept! Their professionalism shows in management, quality of work and customer service.”',
+      '“We get here the best quality products at a really good affordable price.”',
   },
   {
     name: 'Karim Ahmed',
     position: 'Architect',  
     photo: 'https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bGFwdG9wfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=600&q=60',
     text:
-      "“Our architect and several subs have specifically mentioned how much they enjoyed working with SP Construction. I was also very pleased with the timeliness of the construction. It was a pleasure to work with professionals that treated the project and jobsite as if it were their own property and building.”",
+      "“My wife loved the T.V she just purchased from the Comfort Home store.”",
   },
   {
-    name: 'Renee Sims',
-    position: 'HR Manager',
+    name: 'Asad Ahmed',
+    position: 'Researcher',
     photo: 'https://images.unsplash.com/photo-1591543620767-582b2e76369e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=600&q=60',
     text:
-      "“During the course of our project,SP Construction maintained the highest standards of quality, value, professionalism, safety, and cleanliness. Our project was completed on schedule and under budget. ",
+      "“Got the right telescope for my research. ",
   },
-  {
-    name: 'Jonathan Nunfiez',
-    position: 'Graphic Designer',
-    photo: 'https://randomuser.me/api/portraits/men/43.jpg',
-    text:
-      "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
-  },
-  {
-    name: 'Rohit Sharma',
-    position: 'Senior Accountant',
-    photo:
-      'https://media.istockphoto.com/photos/low-angle-of-tall-building-in-manhattan-picture-id1291177121?b=1&k=20&m=1291177121&s=170667a&w=0&h=aI1PkWS_GkXJ4Qz0gz2cTOud95SS4hUa4iZc1kqWYL4=',
-    text:
-      'I want to say that the working relationship between the building committee that  assimilated at that time and the entire SP Construction team could not have been more collaborative or productive',
-  },
-  {
-    name: 'Veeti Seppanen',
-    position: 'Project Director',
-    photo: 'https://randomuser.me/api/portraits/men/97.jpg',
-    text:
-      'Throughout the duration of the project from design to actual construction the representatives of SP Construction have conducted themselves with the utmost professionalism and extended every courtesy possible to my group.',
-  },
+  
 ]
 
 let idx = 1
